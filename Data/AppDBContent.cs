f0c9051c09e13d7b8aebde8a4ccc63b133acee24
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2026-06-12 15:43:28 +0300

    dsd

--- Data/AppDBContent.cs
+++ Data/AppDBContent.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿ using Microsoft.EntityFrameworkCore;
 using Proba.Data.Models;
 
 
--- obj/Debug/net8.0/Proba.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507154c719cdff00f200916eaf41d26b0514ff7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff403a07b2498d5da4aca96607a2d25a048c8a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
